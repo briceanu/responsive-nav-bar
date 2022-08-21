dc7949fe6333f3e538e9e29fadc5e5f50cd7c764
Teodor <ursus_24@yahoo.com>
2022-08-21 04:26:36 +0300

    add the responsive part

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
- 
+let nav_bar = document.querySelector(".nav-bar");
+let toggle_button = document.querySelector(".toggle-button");
+toggle_button.addEventListener("click", () => {
+  nav_bar.classList.toggle("toggle");
+});
